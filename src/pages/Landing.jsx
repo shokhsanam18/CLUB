a9604f7f9b1697acff636e79edaed480b0862d4f
InobatMi <inobatmirahmedova@gmail.com>
2025-06-19 18:28:20 +0500

    fix: absolute positioning

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -9,7 +9,7 @@ export default function MainPage() {
       <section style={{
         backgroundImage: "url('/bg-image.png')",
       }}
-        className="relative h-[85vh] flex flex-col bg-cover bg-bottom justify-center items-center text-center text-white bg-zinc-900 py-24 px-4 md:px-12">
+        className="relative h-[85vh] flex flex-col bg-cover bg-bottom-right justify-center items-center text-center text-white bg-zinc-900 py-24 px-4 md:px-12">
         <div className="absolute inset-0 opacity-60"></div>
         <div className="relative">
           <h1 className="text-5xl md:text-7xl font-bold mb-4">
@@ -20,7 +20,7 @@ export default function MainPage() {
           </p>
           <Link to={'/Clubs'}>
 
-            <Button variant="gradient" size="sm" className="bg-cover font-['Silkscreen'] cursor-pointer uppercase font-light text-xl text-[#77C042] bg-bottom"
+            <Button variant="gradient" size="sm" className="bg-cover font-['Silkscreen'] px-6 py-4 cursor-pointer uppercase font-light text-xl text-[#77C042] bg-bottom"
               style={{
                 backgroundImage: "url('/form.png')",
               }}
@@ -29,9 +29,9 @@ export default function MainPage() {
             </Button>
           </Link>
         </div>
-        <img src="/decoration1.png" alt="" className="absolute bottom-1 left-4 w-3xl" />
-        <img src="/decoration3.png" alt="" className="absolute bottom-16 right-4 w-40" />
-        <img src="/decoration3.png" alt="" className="absolute bottom-8 right-55 w-40" />
+        <img src="/decoration1.png" alt="" className="absolute bottom-1 left-[3%] w-[60vw]" />
+        <img src="/decoration3.png" alt="" className="absolute bottom-[11vh] right-[1%] w-[13vw]" />
+        <img src="/decoration3.png" alt="" className="absolute bottom-[5vh] right-[18%] w-[13vw]" />
       </section>
 
       {/* Universities Section */}

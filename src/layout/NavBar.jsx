f7731495c4e1204f2931715b7a559efee027ff46
InobatMi <inobatmirahmedova@gmail.com>
2025-06-19 18:19:11 +0500

    fix: navbar in mobile fixed

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -20,7 +20,7 @@ export default function NavBar() {
     }, []);
 
     const navList = (
-        <ul className="mb-4 mt-2 font-semibold flex text-xl flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+        <ul className="mb-2 mt-1 font-semibold flex text-xl  gap-2 lg:mb-0 lg:mt-0 flex-row items-center lg:gap-6">
             <Typography
                 as="li"
                 variant="small"
@@ -74,7 +74,7 @@ export default function NavBar() {
                 </Link>
                 <div className="mr-4 hidden lg:block">{navList}</div>
 
-                <div className="flex items-center gap-5">
+                <div className="lg:flex items-center hidden gap-5">
                     <Link to={'/Registration'}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8">
                             <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
@@ -94,7 +94,7 @@ export default function NavBar() {
                 </div>
                 <IconButton
                     variant="text"
-                    className="lg:hidden"
+                    className="lg:hidden cursor-pointer items-center justify-center flex px-6"
                     onClick={() => setOpenNav(!openNav)}
                 >
                     {openNav ? (
@@ -104,9 +104,15 @@ export default function NavBar() {
                     )}
                 </IconButton>
             </div>
-            <Collapse open={openNav}>
+            <Collapse open={openNav} className="flex items-center gap-3 justify-end">
                 {navList}
-                <Button variant="gradient" size="sm" className="bg-cover uppercase flex items-center justify-center text-[#77C042] bg-center"
+                <Link to={'/Registration'}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8">
+                            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
+                        </svg>
+                    
+                    </Link>
+                <Button variant="gradient" size="sm" className="bg-cover uppercase flex items-center justify-center text-[#77C042] bg-bottom-right"
                     style={{
                         backgroundImage: "url('/form.png')",
                     }}

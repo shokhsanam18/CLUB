24b35fc25d8efeddd5e3578efe6f04d599ae3a5b
shokhsanam18 <shokhsanam18@gmail.com>
2025-07-12 14:22:45 +0500

    fix

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const News = () => {
   return (
-    <div>News</div>
-  )
-}
+    <div className="w-full min-h-screen">
+      <section
+        style={{
+          backgroundImage: "url('/bg-image.png')",
+        }}
+        className="relative h-[85vh] flex flex-col bg-cover bg-bottom-right justify-center items-center text-center text-white bg-zinc-900 py-24 px-4 md:px-12"
+      >
+        <div className="absolute inset-0 opacity-60"></div>
+        <div className="relative">
+          <h1 className="text-5xl md:text-7xl font-bold mb-4 ">
+            Rediscover Yourself:
+          </h1>
+          <p className="text-2xl md:text-4xl font-bold mb-6">
+            Find Your Club, Find Your People!
+          </p>
+          <Link to={"/Clubs"}>
+            <Button
+              variant="gradient"
+              size="sm"
+              className="bg-cover hover:scale-90 hover:ease-in-out hover:transition-colors hover:duration-300  font-['Silkscreen'] px-6 py-4 cursor-pointer uppercase font-light text-xl text-[#77C042] bg-bottom rounded-none"
+              style={{
+                backgroundImage: "url('/form.png')",
+              }}
+            >
+              Explore clubs
+            </Button>
+          </Link>
+        </div>
+        <img
+          src="/decoration1.png"
+          alt=""
+          className="absolute bottom-1 left-[3%] w-[60vw]"
+        />
+        <img
+          src="/decoration3.png"
+          alt=""
+          className="absolute bottom-[11vh] right-[1%] w-[13vw]"
+        />
+        <img
+          src="/decoration3.png"
+          alt=""
+          className="absolute bottom-[5vh] right-[18%] w-[13vw]"
+        />
+      </section>
+    </div>
+  );
+};
 
-export default News
+export default News;

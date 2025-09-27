e33066e0bcb0c8c5316ecb2dd0e84d3f1cf6ebc7
dmitriy4kim <dmitrijkim243@gmail.com>
2025-09-27 22:18:39 +0500

    vite config target change

--- club_frontend/vite.config.js
+++ club_frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         },
         proxy: {
             "/api": {
-                target: "http://localhost:8000",
+                target: "https://itcomclubs.uz",
                 changeOrigin: true,
                 secure: false,
             },

36de5647c83edcd8ece91b1abe82914c8351d8ae
A133uz <matrix.valiev2005@gmail.com>
2025-09-30 18:07:13 +0500

    corrected csrf token handling

--- club_backend/club_backend/settings.py
+++ club_backend/club_backend/settings.py
@@ -189,8 +189,6 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.MultiPartParser', 
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
